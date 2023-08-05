1479164632c9155e646b389d4c18c00f8c6066fe
Nikolay <gulyaev132415@yandex.ru>
2023-08-05 20:20:08 +0500

    2

--- statistics/server/src/main/java/ru/practicum/StatsProjection.java
+++ statistics/server/src/main/java/ru/practicum/StatsProjection.java
@@ -2,8 +2,12 @@ package ru.practicum;
 
 public interface StatsProjection {
     String getApp();
+<<<<<<< HEAD
 
     String getUri();
 
+=======
+    String getUri();
+>>>>>>> df10b42 (2)
     Long getHits();
 }
