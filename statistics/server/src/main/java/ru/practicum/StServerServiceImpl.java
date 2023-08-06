b24ad46f5946199000c4af19658f377cdd642650
Nikolay <gulyaev132415@yandex.ru>
2023-08-06 15:11:04 +0500

    2

--- statistics/server/src/main/java/ru/practicum/StServerServiceImpl.java
+++ statistics/server/src/main/java/ru/practicum/StServerServiceImpl.java
@@ -8,7 +8,6 @@ import ru.practicum.model.EndpointHit;
 import ru.practicum.model.ViewStatsDto;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -26,16 +25,15 @@ public class StServerServiceImpl implements StServerService {
 
     @Override
     public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, boolean unique, List<String> uris) {
-        if(uris==null) uris= Collections.emptyList();
-            if (unique && !uris.isEmpty()) {
-                return stRepository.getUrisWithUniqueIP(start, end, uris);
-            } else if (!uris.isEmpty()) {
-                return stRepository.getUrisStats(start, end, uris);
-            } else if (unique) {
-                return stRepository.getAllUrisWithUniqueIP(start, end);
-            } else {
-                return stRepository.getAllUrisStats(start, end);
-            }
+        if (uris != null && unique && !uris.isEmpty()) {
+            return stRepository.getUrisWithUniqueIP(start, end, uris);
+        } else if (uris != null && !uris.isEmpty()) {
+            return stRepository.getUrisStats(start, end, uris);
+        } else if (unique) {
+            return stRepository.getAllUrisWithUniqueIP(start, end);
+        } else {
+            return stRepository.getAllUrisStats(start, end);
+        }
     }
 
 }
