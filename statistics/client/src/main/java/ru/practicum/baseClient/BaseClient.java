fb88ac2565bd0a9359fcc4e5dae82ade918c3659
Nikolay <gulyaev132415@yandex.ru>
2023-08-15 11:16:12 +0500

    2

--- statistics/client/src/main/java/ru/practicum/baseClient/BaseClient.java
+++ statistics/client/src/main/java/ru/practicum/baseClient/BaseClient.java
@@ -1,14 +1,14 @@
 package ru.practicum.baseClient;
 
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
@@ -23,21 +23,33 @@ public class BaseClient {
         this.rest = rest;
     }
 
+    private static ResponseEntity<Object> prepareGatewayResponse(ResponseEntity<Object> response) {
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response;
+        }
+
+        ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.status(response.getStatusCode());
+
+        if (response.hasBody()) {
+            return responseBuilder.body(response.getBody());
+        }
+
+        return responseBuilder.build();
+    }
 
     protected ResponseEntity<Object> get(LocalDateTime start, LocalDateTime end, boolean unique, List<String> uris) {
-        return makeAndSendRequest(start,end,unique,uris);
+        return makeAndSendRequest(start, end, unique, uris);
     }
 
     protected <T> ResponseEntity<Object> post(String path, T body) {
         return post(path, null, null, body);
     }
 
-
     protected <T> ResponseEntity<Object> post(String path, Long userId, @Nullable Map<String, Object> parameters, T body) {
         return makeAndSendRequest(HttpMethod.POST, path, userId, parameters, body);
     }
 
-    private  <T> ResponseEntity<Object> makeAndSendRequest(LocalDateTime start, LocalDateTime end, boolean unique, List<String> uris)  {
+    private <T> ResponseEntity<Object> makeAndSendRequest(LocalDateTime start, LocalDateTime end, boolean unique, List<String> uris) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String st = URLEncoder.encode(start.format(formatter), StandardCharsets.UTF_8);
         String en = URLEncoder.encode(end.format(formatter), StandardCharsets.UTF_8);
@@ -51,11 +63,8 @@ public class BaseClient {
         String finalUrl = builder.toUriString();
 
         ResponseEntity<Object> response = rest.getForEntity(finalUrl, Object.class);
-      return   ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
     }
-
-
-
 
     private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, Long userId, @Nullable Map<String, Object> parameters, @NonNull T body) {
         HttpEntity<T> requestEntity = new HttpEntity<>(body);
@@ -70,19 +79,5 @@ public class BaseClient {
             return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
         }
         return prepareGatewayResponse(shareitServerResponse);
-    }
-
-    private static ResponseEntity<Object> prepareGatewayResponse(ResponseEntity<Object> response) {
-        if (response.getStatusCode().is2xxSuccessful()) {
-            return response;
-        }
-
-        ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.status(response.getStatusCode());
-
-        if (response.hasBody()) {
-            return responseBuilder.body(response.getBody());
-        }
-
-        return responseBuilder.build();
     }
 }
